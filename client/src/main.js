71c2b4c1df9e1c2e3f3ab89830cc3cfebf50fbeb
Hennadii Kovryzhenko <hkovryzhenko@gmail.com>
2021-07-27 10:42:35 +0300

    Implemented general-purpose context menu
    Added a base component for a general-purpose context menu.

--- client/src/main.js
+++ client/src/main.js
@@ -4,8 +4,11 @@ const app = createApp(App);
 
 import router from "./router";
 import store from "./store/index";
+import BaseContextMenu from "./components/base/BaseContextMenu.vue";
 
 app.use(store);
 app.use(router);
+
+app.component("base-context-menu", BaseContextMenu);
 
 app.mount("#app");
